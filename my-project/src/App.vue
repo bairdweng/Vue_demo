4d8555115ac6f4c469276cd24a3e02babc1dc257
Baird-weng <644672334@qq.com>
2016-12-20 19:34:07 +0800

    我的vue

---
+++ my-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <page></page>
+</template>
+<script>
+import Hello from './components/Hello'
+import page from './components/page'
+export default {
+  components: {
+    Hello,
+    page
+  }
+}
+</script>
+<style>
+</style>
